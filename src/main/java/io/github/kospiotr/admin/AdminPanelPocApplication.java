5702f46397c1f810f0f55f2e44671a09bd865d20
Piotr Kosmowski <kospiotr@gmail.com>
2016-02-29 09:40:46 +0100

    Add missing files

---
+++ src/main/java/io/github/kospiotr/admin/AdminPanelPocApplication.java
@@ -0,0 +1,12 @@
+package io.github.kospiotr.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPanelPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPanelPocApplication.class, args);
+	}
+}
